1b293f184c603eed0edad9cf8cb1f6bb92a6fe27
ntbog <wakasaga@gmail.com>
2020-02-18 11:03:16 -0800

    Updates

---
+++ precache-manifest.9b7f16591167a929ab9b6fa54faffcb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f10234f778199395ba510bd6ec3e631",
+    "url": "/burger-app/index.html"
+  },
+  {
+    "revision": "74efe7f1d69be7ba887d",
+    "url": "/burger-app/static/css/main.8d804b38.chunk.css"
+  },
+  {
+    "revision": "d9b4608aa4c24c85df91",
+    "url": "/burger-app/static/js/2.071fcf82.chunk.js"
+  },
+  {
+    "revision": "74efe7f1d69be7ba887d",
+    "url": "/burger-app/static/js/main.dc680573.chunk.js"
+  },
+  {
+    "revision": "5d72ccdd334aa1b54c0b",
+    "url": "/burger-app/static/js/runtime-main.248891f2.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
